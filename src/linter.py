7d62c019b96e57e30b238ae9f417756e8ae0e1a0
Kyle <Kyle.rogers533@gmail.com>
2015-11-14 19:52:11 -0600

    Add case for wrong bracket placement

--- src/linter.py
+++ src/linter.py
@@ -12,6 +12,12 @@ with open(sys.argv[1], 'r') as js:
             print("%d. Statement should end with a semicolon." % lineNum)
         if(not re.match(".*[^\s+]$", line)):
             print("%d. Statement should not have trailing whitespace." % lineNum)
+        if '{' in line:
+            if(not re.match(".+\{\\n$",line)):
+                print("%d. Open curly brace should not stand-alone." % lineNum)
+        if '}' in line:
+            if(not re.match("\}\\n$", line)):
+                print("%d . Closing curly brace should stand-alone." % lineNum)
         lastLine = line
     if(not re.match(".*\\n$", lastLine)):
         print("%d. File %s should end with a newline character." % (lineNum, sys.argv[1]))
