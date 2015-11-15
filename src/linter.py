8b99a35021cc39ecf0e36decbb5fbd889dca0750
Kyle <Kyle.rogers533@gmail.com>
2015-11-14 19:40:52 -0600

    Add test case for trailing whitespace and newline at the end of the file

--- src/linter.py
+++ src/linter.py
@@ -1,13 +1,18 @@
 import re
+import sys
 
 with open(sys.argv[1], 'r') as js:
     lineNum = 0
+    lastLine = ''
     for line in js:
-        if(re.match("\/\/.*", line)):
-            lineNum++
+        lineNum += 1
+        if(re.match("\/\/.*", line) or re.match("^\s*$", line)):
             continue
-        if(!re.match(".*;|{$", line)):
-            print ('%d. Statement should end with a semicolon.', lineNum)
-
-        lineNum++
+        if(not re.match("^.*[\;|\}|\{]$", line)):
+            print("%d. Statement should end with a semicolon." % lineNum)
+        if(not re.match(".*[^\s+]$", line)):
+            print("%d. Statement should not have trailing whitespace." % lineNum)
+        lastLine = line
+    if(not re.match(".*\\n$", lastLine)):
+        print("%d. File %s should end with a newline character." % (lineNum, sys.argv[1]))
 js.closed
